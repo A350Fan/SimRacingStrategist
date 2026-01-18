d53a286a066ccb2100f7b83b1f3a988f5ca654a3
A350Fan <193820755+A350Fan@users.noreply.github.com>
2026-01-18 12:17:46 +0100

    added game profiles for F1 2017-2019 and F1 2021-24

--- app/game_profiles.py
+++ app/game_profiles.py
@@ -42,10 +42,97 @@ GAME_PROFILES = {
         has_minisectors=True,
     ),
 
+    "F1_24": GameProfile(
+        key="F1_24",
+        name="F1 24",
+        packet_format=2021,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=False,
+        has_sector_start_distances=True,
+        has_minisectors=True,
+    ),
+
+    "F1_23": GameProfile(
+        key="F1_23",
+        name="F1 23",
+        packet_format=2021,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=False,
+        has_sector_start_distances=True,
+        has_minisectors=True,
+    ),
+
+
+    "F1_22": GameProfile(
+        key="F1_22",
+        name="F1 22",
+        packet_format=2021,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=False,
+        has_sector_start_distances=True,
+        has_minisectors=True,
+    ),
+
+
+    "F1_21": GameProfile(
+        key="F1_21",
+        name="F1 2021",
+        packet_format=2021,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=False,
+        has_sector_start_distances=True,
+        has_minisectors=True,
+    ),
+
+
     "F1_2020": GameProfile(
         key="F1_2020",
         name="F1 2020",
         packet_format=2020,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=True,
+        has_sector_start_distances=False,
+        has_minisectors=False,
+    ),
+
+    "F1_2019": GameProfile(
+        key="F1_2019",
+        name="F1 2019",
+        packet_format=2019,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=True,
+        has_sector_start_distances=False,
+        has_minisectors=False,
+    ),
+
+    "F1_2018": GameProfile(
+        key="F1_2018",
+        name="F1 2018",
+        packet_format=2018,
+        header_size=24,
+        lap_car_size=53,
+        car_status_size=60,
+        lap_time_is_float=True,
+        has_sector_start_distances=False,
+        has_minisectors=False,
+    ),
+
+    "F1_2017": GameProfile(
+        key="F1_2017",
+        name="F1 2017",
+        packet_format=2017,
         header_size=24,
         lap_car_size=53,
         car_status_size=60,
